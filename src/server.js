c3a17fb81ec192ee0916ab4a3b0e3b531eb934a1
Satvik <satviksharma511@gmail.com>
2026-03-06 18:47:06 +0530

    updated server.js

--- src/server.js
+++ src/server.js
@@ -6,28 +6,28 @@ import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./db/connections.js";
 import apiRoutes from "./routes/api.js";
-
+import userRoutes from "./routes/user.js"; 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+
 const tempDir = path.join(__dirname, "../temp");
 if (!fs.existsSync(tempDir)) {
   fs.mkdirSync(tempDir);
 }
 
-
 connectDB();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// Middleware
+app.use(cors()); 
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-
+// Routes
 app.use("/api", apiRoutes);
-
-
+app.use("/api/users", userRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
