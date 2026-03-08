5486800062d8404b2357ae90a233b34d33d4f011
Satvik <satviksharma511@gmail.com>
2026-03-08 13:36:10 +0530

    server.js update

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./db/connections.js";
 import apiRoutes from "./routes/api.js";
-import userRoutes from "./routes/user.js"; 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,14 +19,11 @@ if (!fs.existsSync(tempDir)) {
 connectDB();
 const app = express();
 
-// Middleware
 app.use(cors()); 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api", apiRoutes);
-app.use("/api/users", userRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
